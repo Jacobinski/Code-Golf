f2ceef768b9eb1b9cd06d025dcce88f504eb2ee1
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-10-18 19:34:58 -0600

    Golfed 8 bytes off Hopping Numbers on Oct 18, 2017

--- C/145590_hopping_numbers.c
+++ C/145590_hopping_numbers.c
@@ -1,1 +1,1 @@
-p,l,d,i=11;main(){char s[10];while(i<123456789){p=0;sprintf(s,"%d",++i);l=strlen(s)-1;for(;l>0;l--){d=s[l]-s[l-1];if(p==0)p=d;if(p^d|d<1)break;p=d;}if(l==0)puts(s);}}
+p,l,d,i=11;main(){for(char s[10];i<=1e9;){sprintf(s,"%d",++i);p=0;for(l=strlen(s);--l>0;){d=s[l]-s[l-1];if(!p)p=d;if(p^d|d<1)break;p=d;}if(!l)puts(s);}}
